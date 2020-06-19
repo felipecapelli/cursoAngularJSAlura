988bca28930cdc8ae7d6200571b3f7254e7d440e
felipecapelli <felipe.ccapelli@gmail.com>
2020-06-19 14:38:57 -0300

    Aula 8

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,18 +1,40 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http){
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
     $scope.foto = {};
+
+    if($routeParams.fotoId){
+        $http.get('v1/fotos/'+ $routeParams.fotoId)
+        .success(function(foto){
+            $scope.foto = foto;
+        })
+        .error(function(erro){
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível obter a foto de ID ' + $routeParams.fotoId;
+        });
+    }
 
     $scope.submeter = function(){
         if($scope.formulario.$valid){
-            $http.post('v1/fotos', $scope.foto)
-            .success(function(){
-                $scope.foto = {};
-                $scope.mensagem = 'Foto Cadastrada com sucesso';
-                //console.log($scope.formulario.titulo.$name);//se quiser também pode acessar o item do formulario pelo name, sem precisar usar o data binding com ng-model
-            })
-            .error(function(erro){
-                $scope.mensagem = 'Não foi possível incluir a foto';
-                console.log(erro);
-            });
+            if($scope.foto._id){
+                $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                .success(function(){
+                    $scope.mensagem = 'A foto ' + $scope.foto + ' foi alterada com sucesso';
+                })
+                .error(function(error){
+                    console.log(error);
+                    $scope.mensagem = 'Não foi Possível alterar a foto '+ $scope.foto.titulo;
+                })
+            }else{
+                $http.post('v1/fotos', $scope.foto)
+                .success(function(){
+                    $scope.foto = {};
+                    $scope.mensagem = 'Foto Cadastrada com sucesso';
+                    //console.log($scope.formulario.titulo.$name);//se quiser também pode acessar o item do formulario pelo name, sem precisar usar o data binding com ng-model
+                })
+                .error(function(erro){
+                    $scope.mensagem = 'Não foi possível incluir a foto';
+                    console.log(erro);
+                });
+            }
         }
     }
 });
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -17,4 +17,17 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
         console.log(error);
     });*/
 
+    $scope.remover = function(foto){
+        $http.delete('v1/fotos/' + foto._id)
+        .success(function(){
+            var indiceFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indiceFoto, 1);
+            $scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso';
+        })
+        .error(function(erro){
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível remover a foto' + foto.titulo;
+        })
+    };
+
 });
